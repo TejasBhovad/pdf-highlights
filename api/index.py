5a77d3f78886b249a5cbcdb03f584c83dd983015
TejasBhovad <tejasbhovad@gmail.com>
2024-08-26 22:05:10 +0530

    fix: directly return binary

--- api/index.py
+++ api/index.py
@@ -1,11 +1,14 @@
+import logging
+import os
 from io import BytesIO
 
-import requests
-from flask import Flask, request, url_for, jsonify
-import os
 import pymupdf
+import requests
+from flask import Flask, send_file
+from flask import jsonify, request, url_for
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
+
 from api.utils.file import (
     generate_clustered_content_md,
     extract_highlighted_text_with_line_numbers,
@@ -14,7 +17,6 @@ from api.utils.file import (
     get_highlighted_pages,
     extract_highlighted_text_with_line_numbers_on_pages
 )
-import logging
 
 app = Flask(__name__)
 limiter = Limiter(key_func=get_remote_address)
@@ -83,13 +85,6 @@ def generate_pdf():
     return jsonify({"url": url_for('static', filename='highlighted_content.pdf', _external=True)})
 
 
-from flask import jsonify, request, url_for
-import logging
-import pymupdf
-import requests
-from io import BytesIO
-import os
-
 @app.route('/api/get-highlighted-page')
 @limiter.limit("5 per minute")  # Rate limit
 def get_highlighted_page():
@@ -112,25 +107,24 @@ def get_highlighted_page():
         if not pages_list:
             return jsonify({"error": "No highlighted pages found."}), 404
 
-        # Ensure the static directory exists
-        base_dir = os.path.dirname(os.path.abspath(__file__))
-        static_dir = os.path.join(base_dir, 'static')
-        if not os.path.exists(static_dir):
-            os.makedirs(static_dir)
+        # Create a BytesIO object for the output PDF
+        output_pdf = BytesIO()
 
-        pdf_file_path = os.path.join(static_dir, 'highlighted_content.pdf')
+        # Extract highlighted text and create a new PDF in memory
+        extract_highlighted_text_with_line_numbers_on_pages(doc, pages_list, output_pdf)
 
-        # Extract highlighted text and save to the PDF file
-        extract_highlighted_text_with_line_numbers_on_pages(doc, pages_list, pdf_file_path)
+        # Move the cursor of the BytesIO object to the beginning
+        output_pdf.seek(0)
 
-    except FileNotFoundError as e:
-        logging.error(f"FileNotFoundError: {str(e)}")
-        return jsonify({"error": str(e)}), 404
+        # Return the PDF as a response
+        return send_file(output_pdf, as_attachment=True, download_name='highlighted_content.pdf',
+                         mimetype='application/pdf')
+
     except Exception as e:
         logging.error(f"Unexpected error: {str(e)}")
         return jsonify({"error": "An unexpected error occurred."}), 500
 
-    return jsonify({"url": url_for('static', filename='highlighted_content.pdf', _external=True)})
+
 @app.route('/api/')
 def hello_world():
     return 'Hello, World'
